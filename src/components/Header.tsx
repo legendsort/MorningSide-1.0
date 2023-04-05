be88998791604eef94823a2b530ca9136be6fed4
magicpalmtree <magicpaltree.space@gmail.com>
2023-04-04 20:21:53 -0400

    added font-awesome

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,29 @@ import React from 'react'
 
 const Header = () => {
 	return (
-		<div className="absolute top-0 left-0">
-			header
+		<div className="absolute top-0 left-0 w-full h-[100px] z-[2]">
+			<div className="container flex items-center gap-[50px]">
+				<img src="/images/logo.png" alt="logo"/>
+				<li>
+					<a href="about.html">Home</a>
+				</li>
+				<li>
+					<a href="about.html">About</a>
+				</li>
+				<li>
+					<a href="about.html">Services</a>
+				</li>
+				<li>
+					<a href="about.html">Pages</a>
+				</li>
+				<li>
+					<a href="about.html">News</a>
+				</li>
+				<li>
+					<a href="about.html">Contact</a>
+				</li>
+				<a href="#" class="theme-btn">CONSULTANCY <i class="fas fa-arrow-right"></i></a>
+			</div>
 		</div>
 	)
 }
